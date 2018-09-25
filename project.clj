9dca7f404d71c8c705f67cdbe1a8884ea72a9f83
Markovic Vladimir <markovic.vladimir86@gmail.com>
2018-09-25 16:17:11 +0200

    Code formatting.

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject org.vladimir/srpski-jezik-lib "0.1.0"
- :description "Srpski jezik library"
- :url "http://gitlab:1610/VladimirMarkovic86/srpski-jezik-lib"
- :license {:name "Eclipse Public License"
-           :url "http://www.eclipse.org/legal/epl-v10.html"}
- :dependencies [[org.clojure/clojure "1.9.0"]
-                [org.vladimir/utils-lib "0.1.0"]
-                ]
- :source-paths ["src/clj"])
+  :description "Srpski jezik library"
+  :url "http://gitlab:1610/VladimirMarkovic86/srpski-jezik-lib"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.vladimir/utils-lib "0.1.0"]
+                 ]
+  :source-paths ["src/clj"])
