3ff746c7b3c7e38ac2da7d98850a5a4d57ade92c
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2018-10-15 17:48:24 +0200

    Prepared project for clojars.

--- project.clj
+++ project.clj
@@ -1,9 +1,13 @@
-(defproject org.vladimir/srpski-jezik-lib "0.1.0"
+(defproject org.clojars.vladimirmarkovic86/srpski-jezik-lib "0.1.0"
   :description "Srpski jezik library"
-  :url "http://gitlab:1610/VladimirMarkovic86/srpski-jezik-lib"
+  :url "http://github.com/VladimirMarkovic86/srpski-jezik-lib"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.vladimir/utils-lib "0.1.0"]
+                 [org.clojars.vladimirmarkovic86/utils-lib "0.1.0"]
                  ]
+
+  :min-lein-version "2.0.0"
+
   :source-paths ["src/clj"])
+
